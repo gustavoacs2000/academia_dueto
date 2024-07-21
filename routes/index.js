8172cd67b1a2c687bc67f27e2d984413611452c6
gustavo alexandre <gustavoacs2000@gmail.com>
2024-07-21 12:56:51 -0300

    routes: subindo rota para tela de reset de senha

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,39 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
 // Landing Page
-router.get('/', (req, res) => {
-    res.render('landing');
+router.get("/", (req, res) => {
+  res.render("landing");
 });
 
 // Login Page
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
+// Reset password Page
+router.get("/forgot-password", (req, res) => {
+  res.render("resetPassword");
+});
+
+// Function to reset password called by the Reset password Page
+router.post("/reset-password", (req, res) => {});
+
 // Register Page
-router.get('/register', (req, res) => {
-    res.render('register');
+router.get("/register", (req, res) => {
+  res.render("register");
 });
 
 // Videos Page
-router.get('/videos', (req, res) => {
-    // Fetch videos from Google Drive or any other source
-    const videos = [
-        { title: 'Video 1', completed: false },
-        { title: 'Video 2', completed: false },
-        // Add more videos here
-    ];
-    res.render('videos', { videos });
+router.get("/videos", (req, res) => {
+  // Fetch videos from Google Drive or any other source
+  const videos = [
+    { title: "Video 1", completed: false },
+    { title: "Video 2", completed: false },
+    // Add more videos here
+  ];
+  res.render("videos", { videos });
 });
 
 export default router;
