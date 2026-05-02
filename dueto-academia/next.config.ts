dde8b91ec4a888825f9f5a10b6c374a05ef223d1
gustavoacs2000 <gustavoacs2000@gmail.com>
2026-05-02 10:42:29 -0300

    feat: add outputFileTracingExcludes to next.config for image handling

--- dueto-academia/next.config.ts
+++ dueto-academia/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["26.217.39.208"],
+  outputFileTracingExcludes: {
+    "/*": ["./public/images/dueto/**/*"],
+  },
 };
 
 export default nextConfig;
